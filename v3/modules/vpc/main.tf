f8d7c4f3928a27e7da7a696536b1e6438ab5ffda
전상현 <sanghyunjun@jeonsanghyeon-ui-MacBookPro.local>
2025-06-24 17:29:33 +0900

    Feat: build NGW / RT for private subnet

--- v3/modules/vpc/main.tf
+++ v3/modules/vpc/main.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "this" {
   enable_dns_hostnames = true
 
   tags = { 
-    Name = "v3-nemo-${var.name}-vpc"
+    Name = "${var.name}-VPC"
   }
 }
 
@@ -24,4 +24,62 @@ resource "aws_subnet" "private" {
   tags = { 
     Name = "${var.name}-private-subnet"
   }
+}
+
+resource "aws_internet_gateway" "this"{
+  vpc_id                = aws_vpc.this.id
+  tags = { 
+    Name = "${var.name}-IGW"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id                = aws_vpc.this.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+  tags = {
+    Name = "${var.name}-public-RT"
+  }
+}
+
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+
+
+
+
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "this" {
+  subnet_id     = aws_subnet.public.id
+  allocation_id = aws_eip.nat.id
+  tags = {
+    Name = "${var.name}-NGW"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id                = aws_vpc.this.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+  tags = {
+    Name = "${var.name}-private-RT"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
 }
