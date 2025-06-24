308a42feda89dfee29f314554cc676dc16c6b96b
전상현 <sanghyunjun@jeonsanghyeon-ui-MacBookPro.local>
2025-06-24 16:26:59 +0900

    Feat: build subnet for this VPC

--- v3/modules/vpc/variables.tf
+++ v3/modules/vpc/variables.tf
@@ -7,3 +7,18 @@ variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
 }
+
+variable "public_subnet_cidr" {
+    description = "public subnet CIDR"
+    type = string
+}
+
+variable "private_subnet_cidr" {
+    description = "private subnet CIDR"
+    type = string
+}
+
+variable "subnet_az" {
+  type        = list(string)
+  description = "Availability zones to use"
+}
