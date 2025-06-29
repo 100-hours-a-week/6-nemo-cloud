f0f3d7035da1a759786613c1471afb9b9ab202ad
전상현 <sanghyunjun@jeonsanghyeon-ui-MacBookPro.local>
2025-06-29 16:38:12 +0900

    Fix: add EBS CLI IAM Role(IRSA) for kafka (#75)

--- v3/modules/eks/main.tf
+++ v3/modules/eks/main.tf
@@ -31,8 +31,9 @@ resource "aws_eks_node_group" "this" {
   ]
 }
 
-
-# EKS Cluster IAM Role
+######################################
+# IAM Roles (기존 클러스터/노드용)
+######################################
 resource "aws_iam_role" "cluster" {
   name = "${var.cluster_name}-eks-cluster-role"
 
@@ -84,3 +85,71 @@ resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
+
+######################################
+# ✅ EBS CSI Driver용 IAM Role (IRSA)
+######################################
+
+resource "aws_iam_policy" "ebs_csi_driver" {
+  name        = "AmazonEKS_EBS_CSI_Driver_Policy"
+  description = "Policy for EBS CSI Driver"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "ec2:AttachVolume",
+          "ec2:CreateSnapshot",
+          "ec2:CreateTags",
+          "ec2:CreateVolume",
+          "ec2:DeleteSnapshot",
+          "ec2:DeleteTags",
+          "ec2:DeleteVolume",
+          "ec2:DescribeInstances",
+          "ec2:DescribeSnapshots",
+          "ec2:DescribeTags",
+          "ec2:DescribeVolumes",
+          "ec2:DescribeAvailabilityZones",
+          "ec2:DetachVolume"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
+# OIDC Provider 생성 (EKS 클러스터에서 추출)
+resource "aws_iam_openid_connect_provider" "this" {
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0afd10df6"]
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+# IRSA용 IAM Role
+resource "aws_iam_role" "ebs_csi_driver" {
+  name = "${var.cluster_name}-ebs-csi-driver"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.this.arn
+      },
+      Action = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringEquals = {
+          "${replace(aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+        }
+      }
+    }]
+  })
+}
+
+# IAM 정책 연결 (EBS 볼륨 제어 권한)
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver" {
+  role       = aws_iam_role.ebs_csi_driver.name
+  policy_arn = aws_iam_policy.ebs_csi_driver.arn
+}
