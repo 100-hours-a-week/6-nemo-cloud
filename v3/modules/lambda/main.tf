37dd55e87ee462be806fad196b7ea7ac17b5dfaf
전상현 <sanghyunjun@jeonsanghyeon-ui-MacBookPro.local>
2025-07-05 16:04:47 +0900

    Feat: Add worker node start/stop control using AWS Lambda(#98)

---
+++ v3/modules/lambda/main.tf
@@ -0,0 +1,50 @@
+resource "aws_iam_role" "lambda_role" {
+  name = var.role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name = var.policy_name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:StartInstances",
+          "ec2:StopInstances",
+          "ec2:DescribeInstances"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+resource "aws_lambda_function" "this" {
+  function_name = var.lambda_name
+  filename      = var.lambda_zip_path
+  handler       = var.handler
+  runtime       = var.runtime
+  role          = aws_iam_role.lambda_role.arn
+
+  environment {
+    variables = var.environment_variables
+  }
+}
