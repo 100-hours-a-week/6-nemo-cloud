78707a76db183be5e5cb3583db7931cc797860ad
halfmoon01 <160722242+halfmoon01@users.noreply.github.com>
2025-07-06 19:59:30 +0900

    Feat: add ArgoCD Image Updater auto-sync integration (100-hours-a-week/6-nemo-be#207)

---
+++ v3/modules/argocd_image_updater/variables.tf
@@ -0,0 +1,25 @@
+variable "kubeconfig_path" {
+  type        = string
+  description = "Path to kubeconfig for Helm provider"
+}
+
+variable "oidc_provider_url" {
+  type        = string
+  description = "OIDC provider URL (from EKS)"
+}
+
+variable "oidc_provider_arn" {
+  type        = string
+  description = "OIDC provider ARN (from IAM OIDC)"
+}
+
+variable "aws_account_id" {
+  type        = string
+  description = "Your AWS Account ID"
+}
+
+variable "region" {
+  type        = string
+  description = "AWS region"
+  default     = "ap-northeast-2"
+}
--- v3/modules/eks/outputs.tf
+++ v3/modules/eks/outputs.tf
@@ -9,3 +9,11 @@ output "cluster_endpoint" {
 output "cluster_ca_certificate" {
   value = aws_eks_cluster.this.certificate_authority[0].data
 }
+
+output "oidc_provider_url" {
+  value = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+output "oidc_provider_arn" {
+  value = aws_iam_openid_connect_provider.this.arn
+}
