382d2e55406be33ffe937c35fec01e96b29f60ab
halfmoon01 <160722242+halfmoon01@users.noreply.github.com>
2025-07-26 19:47:53 +0900

    Refactor: separate sensitive variables to tfvars (tfvars excluded from git)

--- v3/terraform/prod/main.tf
+++ v3/terraform/prod/main.tf
@@ -1,35 +1,35 @@
 module "vpc" {
   source   = "../../modules/vpc"
-  name     = "prod"
-  vpc_cidr = "10.0.0.0/16"
-  public_subnet_cidr = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnet_cidr = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
-  subnet_az = ["ap-northeast-2a", "ap-northeast-2b", "ap-northeast-2c"]
+  name     = var.environment
+  vpc_cidr = var.vpc_cidr
+  public_subnet_cidr = var.public_subnet_cidrs
+  private_subnet_cidr = var.private_subnet_cidrs
+  subnet_az = var.availability_zones
 }
 
 module "eks" {
   source             = "../../modules/eks"
-  cluster_name       = "nemo_EKS_kluster"
-  cluster_version    = "1.33"
+  cluster_name       = var.cluster_name
+  cluster_version    = var.cluster_version
 
   # 이렇게 변수를 가지고 오고싶을때는 VPC 모듈에서 output으로 가지고 와야함. 
   vpc_id             = module.vpc.vpc_id
-  vpc_cidr           = "10.0.0.0/16"
+  vpc_cidr           = var.vpc_cidr
   subnet_ids         = [module.vpc.private_azone_id, module.vpc.private_bzone_id, module.vpc.private_czone_id]
 
-  node_group_name    = "nemo_node_group"
-  desired_capacity   = 3
-  max_capacity       = 3
-  min_capacity       = 3
-  instance_types     = ["t3.large"]
-  key_pair_name      = "keypair-kube-master"  # SSH 접근용 키페어
+  node_group_name    = var.node_group_name
+  desired_capacity   = var.node_desired_capacity
+  max_capacity       = var.node_max_capacity
+  min_capacity       = var.node_min_capacity
+  instance_types     = var.node_instance_types
+  key_pair_name      = var.key_pair_name  # SSH 접근용 키페어
 }
 
 module "ebs_csi_driver" {
   source = "../../modules/ebs"
   
   controller_sa_name      = "ebs-csi-controller-sa"
-  controller_sa_role_arn  = "arn:aws:iam::084375578827:role/nemo_EKS_kluster-ebs-csi-driver"
+  controller_sa_role_arn  = "arn:aws:iam::084375578827:role/${var.cluster_name}-ebs-csi-driver"
   node_sa_name            = "ebs-csi-node-sa"
   kubeconfig_dependency   = null_resource.update_kubeconfig
 }
@@ -51,7 +51,7 @@ module "argocd" {
 
 resource "null_resource" "update_kubeconfig" {
   provisioner "local-exec" {
-    command = "aws eks update-kubeconfig --name nemo_EKS_kluster --region ap-northeast-2"
+    command = "aws eks update-kubeconfig --name ${var.cluster_name} --region ${var.aws_region}"
   }
   ## EKS가 다끝나고 다음 명령어 생성하기 
   depends_on = [module.eks]
@@ -70,7 +70,7 @@ module "lambda_ec2_control" {
 
   environment_variables = {
     ACTION       = "start"
-    INSTANCE_IDS = "i-0657dd55aea798a8e,i-03596f9222f2ea70b,i-0869ce93a892491b9"
+    INSTANCE_IDS = var.ec2_instance_ids
   }
 }
 
@@ -95,13 +95,13 @@ module "rds" {
   source = "../../modules/rds"
 
 
-  name               = "v3-prod-rds"
+  name               = "${var.environment}-rds"
   identifier         = "nemo-db-instance"
-  db_name            = "prod_db"
-  username           = "prod"
-  password           = "Prod1234!"             # 보안상 tfvars에서 관리
-  instance_class     = "db.t3.micro"
-  allocated_storage  = 20
+  db_name            = var.rds_db_name
+  username           = var.rds_username
+  password           = var.rds_password             # 보안상 tfvars에서 관리
+  instance_class     = var.rds_instance_class
+  allocated_storage  = var.rds_allocated_storage
 
   vpc_id             = module.vpc.vpc_id
   eks_security_group_id = module.eks.cluster_security_group_id
@@ -118,18 +118,23 @@ module "bastion" {
   source     = "../../modules/bastion"
 
   vpc_id     = module.vpc.vpc_id
-  instance_type = "t2.medium"
+  instance_type = var.bastion_instance_type
   subnet_id  = module.vpc.public_azone_id
-  ami_id     = "ami-0662f4965dfc70aca"
-  key_name   = "keypair-kube-master"
-  name       = "v3-prod"
+  ami_id     = var.bastion_ami_id
+  key_name   = var.key_pair_name
+  name       = "${var.environment}-bastion"
 }
 
 module "route53" {
-  source      = "../../modules/route53"
-  domain_name = "onurivit01.store"
-  alb_dns_name = "k8s-frontend-frontend-af87f59743-171113497.ap-northeast-2.elb.amazonaws.com"
-  alb_zone_id  = "ZWKZPGTI48KDX"
+  source       = "../../modules/route53"
+  domain_name  = var.domain_name
+  cluster_name = var.cluster_name
+  
+  # ALB가 생성된 후에 실행되도록 의존성 추가
+  depends_on = [
+    module.alb_ingress_controller,
+    null_resource.update_kubeconfig
+  ]
 }
 
 output "ns" {
@@ -139,8 +144,8 @@ output "ns" {
 
 module "alb_ingress_controller" {
   source            = "../../modules/ALB"
-  cluster_name      = "nemo_EKS_kluster"
-  region            = "ap-northeast-2"
+  cluster_name      = var.cluster_name
+  region            = var.aws_region
   vpc_id            = module.vpc.vpc_id
   oidc_provider_arn = module.eks.oidc_provider_arn
   oidc_issuer_url   = module.eks.oidc_issuer_url
@@ -152,7 +157,7 @@ module "argocd_image_updater" {
   oidc_provider_arn = module.eks.oidc_provider_arn
   oidc_provider_url = module.eks.oidc_provider_url
   aws_account_id    = data.aws_caller_identity.current.account_id
-  region            = "ap-northeast-2"
+  region            = var.aws_region
   github_pat        = var.github_pat
   kubeconfig_path   = "~/.kube/config"
   
--- v3/terraform/prod/variables.tf
+++ v3/terraform/prod/variables.tf
@@ -3,3 +3,148 @@ variable "github_pat" {
   type        = string
   sensitive   = true
 }
+
+# VPC Variables
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["ap-northeast-2a", "ap-northeast-2b", "ap-northeast-2c"]
+}
+
+# EKS Variables
+variable "cluster_name" {
+  description = "Name of EKS cluster"
+  type        = string
+  default     = "nemo_EKS_kluster"
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.33"
+}
+
+variable "node_group_name" {
+  description = "Name of EKS node group"
+  type        = string
+  default     = "nemo_node_group"
+}
+
+variable "node_desired_capacity" {
+  description = "Desired number of nodes"
+  type        = number
+  default     = 3
+}
+
+variable "node_max_capacity" {
+  description = "Maximum number of nodes"
+  type        = number
+  default     = 3
+}
+
+variable "node_min_capacity" {
+  description = "Minimum number of nodes"
+  type        = number
+  default     = 3
+}
+
+variable "node_instance_types" {
+  description = "Instance types for nodes"
+  type        = list(string)
+  default     = ["t3.large"]
+}
+
+variable "key_pair_name" {
+  description = "Key pair name for SSH access"
+  type        = string
+  default     = "keypair-kube-master"
+}
+
+# RDS Variables
+variable "rds_password" {
+  description = "Password for RDS instance"
+  type        = string
+  sensitive   = true
+}
+
+variable "rds_username" {
+  description = "Username for RDS instance"
+  type        = string
+  default     = "prod"
+}
+
+variable "rds_db_name" {
+  description = "Database name"
+  type        = string
+  default     = "prod_db"
+}
+
+variable "rds_instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "rds_allocated_storage" {
+  description = "Allocated storage for RDS"
+  type        = number
+  default     = 20
+}
+
+# Lambda Variables
+variable "ec2_instance_ids" {
+  description = "List of EC2 instance IDs to control"
+  type        = string
+  sensitive   = true
+}
+
+# Bastion Variables
+variable "bastion_instance_type" {
+  description = "Instance type for bastion host"
+  type        = string
+  default     = "t2.medium"
+}
+
+variable "bastion_ami_id" {
+  description = "AMI ID for bastion host"
+  type        = string
+  default     = "ami-0662f4965dfc70aca"
+}
+
+# Route53 Variables
+variable "domain_name" {
+  description = "Domain name for Route53"
+  type        = string
+  default     = "onurivit01.store"
+}
+
+# General Variables
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-northeast-2"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
